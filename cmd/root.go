b5267400e787386c274bca31ec29c83712df1c8c
Maximilian Moehl <maximilian.moehl@gmail.com>
2021-04-22 21:23:39 +0200

    bump version to v0.2.2

--- cmd/root.go
+++ cmd/root.go
@@ -35,7 +35,7 @@ Note: Timers cannot overlap. If there are overlapping timers
 the application might fail and statistics or analytics may be wrong.
 
 Note: This cli is NOT concurrency safe.`,
-	Version: "v0.2.1",
+	Version: "v0.2.2",
 }
 
 func init() {

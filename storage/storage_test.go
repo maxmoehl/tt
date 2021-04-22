9d2647892700f3eab42d78b5b561361661494f66
Maximilian Moehl <maximilian.moehl@gmail.com>
2021-04-22 17:28:07 +0200

    add more storage tests

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -1,12 +1,14 @@
 package storage
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 	"time"
+	
+	"github.com/maxmoehl/tt/types"
+	"github.com/maxmoehl/tt/utils"
 
 	"github.com/google/uuid"
-	"github.com/maxmoehl/tt/types"
 )
 
 func TestStartTimer(t *testing.T) {
@@ -51,8 +53,6 @@ func TestStartTimerValidTimestamp(t *testing.T) {
 	timer := testFile.timers[0]
 	if timer.Project != "test" || timer.Task != "" || len(timer.Tags) != 0 ||
 		!timer.Start.Equal(startTime) {
-		fmt.Printf("%s\n", timer.Start.Format(time.RFC3339))
-		fmt.Printf("%s\n", startTime.Format(time.RFC3339))
 		t.Fatal("timer does not match expectations")
 	}
 }
@@ -65,3 +65,187 @@ func TestStartTimerInvalidTimestamp(t *testing.T) {
 	}
 }
 
+func TestStartTimerTimestampCollision(t *testing.T) {
+	testFile.timers = types.Timers{types.Timer{
+		Uuid:    uuid.Must(uuid.NewRandom()),
+		Start:   time.Now().Add(-time.Hour),
+		End:     time.Now(),
+		Project: "test",
+	}}
+	err := StartTimer("test", "", time.Now().Add(-30*time.Minute).Format(time.RFC3339), nil)
+	if err == nil {
+		t.Fatal("expected error because of collision with existing timer")
+	}
+}
+
+func TestStopTimer(t *testing.T) {
+	testFile.timers = types.Timers{types.Timer{
+		Uuid:    uuid.Must(uuid.NewRandom()),
+		Start:   time.Now(),
+		Project: "test",
+	}}
+	err := StopTimer("")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func TestStopTimerNoRunningTimer(t *testing.T) {
+	testFile.timers = nil
+	err := StopTimer("")
+	if err == nil {
+		t.Fatal("expected error because of no running timer")
+	}
+}
+
+func TestStopTimerValidTimestamp(t *testing.T) {
+	testFile.timers = types.Timers{types.Timer{
+		Uuid:    uuid.Must(uuid.NewRandom()),
+		Start:   time.Now().Add(-time.Hour),
+		Project: "test",
+	}}
+	stopTime := time.Now().Round(time.Second)
+	err := StopTimer(stopTime.Format(time.RFC3339))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if len(testFile.timers) != 1 {
+		t.Fatal("expected exactly one timer")
+	}
+	timer := testFile.timers[0]
+	if timer.Project != "test" || timer.Task != "" || len(timer.Tags) != 0 ||
+		!timer.End.Equal(stopTime) {
+		t.Fatal("timer does not match expectations")
+	}
+}
+
+func TestStopTimerInvalidTimestamp(t *testing.T) {
+	testFile.timers = types.Timers{types.Timer{
+		Uuid:    uuid.Must(uuid.NewRandom()),
+		Start:   time.Now().Add(-time.Hour),
+		Project: "test",
+	}}
+	err := StopTimer("invalid timestamp")
+	if err == nil {
+		t.Fatal("expected error because of invalid timestamp")
+	}
+}
+
+func TestStopTimerTimestampBeforeStart(t *testing.T) {
+	testFile.timers = types.Timers{types.Timer{
+		Uuid:    uuid.Must(uuid.NewRandom()),
+		Start:   time.Now(),
+		Project: "test",
+	}}
+	err := StopTimer(time.Now().Add(-time.Hour).Format(time.RFC3339))
+	if err == nil {
+		t.Fatal("expected error because of stop time before start time")
+	}
+}
+
+func TestResumeTimer(t *testing.T) {
+	testFile.timers = types.Timers{types.Timer{
+		Uuid:    uuid.Must(uuid.NewRandom()),
+		Start:   time.Now().Add(-time.Hour),
+		End:     time.Now(),
+		Project: "testA",
+		Task:    "testB",
+		Tags:    []string{"a", "b"},
+	}}
+	timerResume, err := ResumeTimer()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if len(testFile.timers) != 2 {
+		t.Fatalf("expected two timers but got %d", len(testFile.timers))
+	}
+	timerFile := testFile.timers[1]
+	if !reflect.DeepEqual(timerResume, timerFile) {
+		t.Fatal("expected returned timer and timer in storage to be equal")
+	}
+	if timerResume.Project != "testA" ||
+		timerResume.Task != "testB" ||
+		len(timerResume.Tags) != 2 ||
+		!utils.StringSliceContains(timerResume.Tags, "a") ||
+		!utils.StringSliceContains(timerResume.Tags, "b") {
+		t.Fatal("expected properties to be copied but found different values")
+	}
+}
+
+func TestResumeTimerNoTimerToResume(t *testing.T) {
+	testFile.timers = nil
+	_, err := ResumeTimer()
+	if err == nil {
+		t.Fatal("expected error because there is no timer to resume")
+	}
+}
+
+func TestResumeTimerRunningTimer(t *testing.T) {
+	testFile.timers = types.Timers{types.Timer{
+		Uuid:    uuid.Must(uuid.NewRandom()),
+		Start:   time.Now(),
+		Project: "test",
+	}}
+	_, err := ResumeTimer()
+	if err == nil {
+		t.Fatal("expected error because there is a running timer")
+	}
+}
+
+func TestCheckRunningTimers(t *testing.T) {
+	runningId := uuid.Must(uuid.NewRandom())
+	testFile.timers = types.Timers{
+		types.Timer{
+			Uuid:    uuid.Must(uuid.NewRandom()),
+			Start:   time.Now().Add(-time.Hour),
+			End:     time.Now().Add(-45*time.Minute),
+			Project: "test",
+		},
+		types.Timer{
+			Uuid:    runningId,
+			Start:   time.Now().Add(-30*time.Minute),
+			Project: "test",
+		},
+	}
+	ids, err := CheckRunningTimers()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if len(ids) != 1 {
+		t.Fatalf("expected to find one running timer but got %d", len(ids))
+	}
+	if ids[0] != runningId {
+		t.Fatal("returned id is not id of running timer")
+	}
+}
+
+func TestGetRunningTimer(t *testing.T) {
+	testFile.timers = types.Timers{types.Timer{
+		Uuid:    uuid.Must(uuid.NewRandom()),
+		Start:   time.Now(),
+		Project: "test",
+	}}
+	running, timer, err := GetRunningTimer()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if !running {
+		t.Fatal("expected to get running timer")
+	}
+	if timer.Project != "test" ||
+		timer.Task != "" ||
+		timer.Tags != nil {
+		t.Fatal("timer does not match expectations")
+	}
+}
+
+func TestGetRunningTimerNoTimer(t *testing.T) {
+	testFile.timers = nil
+	running, _, err := GetRunningTimer()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if running {
+		t.Fatal("did not expect running timer")
+	}
+}

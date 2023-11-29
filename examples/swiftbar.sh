908bb95fec78c3987a2e2cb134cdcd4e356a8bd3
Maximilian Moehl <maximilian@moehl.eu>
2023-11-29 10:26:09 +0100

    doc: add swiftbar

---
+++ examples/swiftbar.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# <swiftbar.hideAbout>true</swiftbar.hideAbout>
+# <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
+# <swiftbar.hideLastUpdated>true</swiftbar.hideLastUpdated>
+# <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
+# <swiftbar.hideSwiftBar>true</swiftbar.hideSwiftBar>
+# <swiftbar.schedule>* * * * *</swiftbar.schedule>
+
+# optionally configure tt
+# export TT_HOME_DIR="${HOME}/.config/tt"
+
+# path to tt binary, adjust to your system if needed
+export TT_BIN="${HOME}/go/bin/tt"
+
+# print short status to show in menu bar
+"${TT_BIN}" status --short
+# menu options
+echo "---"
+echo "Resume | bash='${TT_BIN}' param0='start' param1='--resume' terminal=false"
+echo "Start | bash='${TT_BIN}' param0='start' terminal=true"
+echo "Stop | bash='${TT_BIN}' param0='stop' terminal=false"
